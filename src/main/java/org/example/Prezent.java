822a68d17aed3ad749b180fc5cc4e25025e46ad0
Lesxcx <szydlowski.konrad1999@gmail.com>
2023-02-20 20:07:06 +0100

    Dodanie pól do klasy prezent, stworzenie konstruktora oraz geterów

--- src/main/java/org/example/Prezent.java
+++ src/main/java/org/example/Prezent.java
@@ -1,5 +1,19 @@
 package org.example;
 
 public class Prezent {
+    private String nazwa;
+    private double waga;
 
+    public Prezent(String nazwa, double waga) {
+        this.nazwa = nazwa;
+        this.waga = waga;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public double getWaga() {
+        return waga;
+    }
 }
