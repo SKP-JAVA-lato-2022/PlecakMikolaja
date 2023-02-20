df9a8bbe9680107af73e39f980e79287917eeb86
Lesxcx <szydlowski.konrad1999@gmail.com>
2023-02-20 20:45:39 +0100

    Dodanie pól do klasy Pakowalnia oraz stworzenie konstruktora, który będzie odpowiadał za działanie programu

--- src/main/java/org/example/Pakowalnia.java
+++ src/main/java/org/example/Pakowalnia.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Pakowalnia {
+    private HashMap<Integer, Prezent> magazynPrezentow;
+    private final double pojemnoscPlecaka = 4.0;
+    private int iloscKursow = 0;
+
+    Magazyn magazyn = new Magazyn(magazynPrezentow);
+
+    public Pakowalnia() {
+        int obecnaWaga = 0;
+        for (int i = 0; i < magazynPrezentow.size(); i++) {
+            double waga = magazynPrezentow.get(i).getWaga();
+            if(obecnaWaga + waga <= pojemnoscPlecaka) {
+                obecnaWaga += waga;
+            } else {
+                iloscKursow++;
+                obecnaWaga = 0;
+            }
+        }
+
+        System.out.println(iloscKursow);
+    }
 }
