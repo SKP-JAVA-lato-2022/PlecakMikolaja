6b143ed05715013f9bbf06ab6ffd4ef29fc4d6e0
Lesxcx <szydlowski.konrad1999@gmail.com>
2023-02-20 20:11:33 +0100

    Stworzenie listy w klasie Magazyn, która będzie przechowywać prezenty do rozwiezienia

--- src/main/java/org/example/Magazyn.java
+++ src/main/java/org/example/Magazyn.java
@@ -3,5 +3,15 @@ package org.example;
 import java.util.ArrayList;
 
 public class Magazyn {
+    private ArrayList<Prezent> magazynPrezentow;
 
+    public Magazyn(ArrayList<Prezent> magazynPrezentow) {
+        this.magazynPrezentow.add(new Prezent("Miś", 0.54));
+        this.magazynPrezentow.add(new Prezent("Konik", 2.59));
+        this.magazynPrezentow.add(new Prezent("Lalka", 0.87));
+        this.magazynPrezentow.add(new Prezent("Sukienka", 0.24));
+        this.magazynPrezentow.add(new Prezent("Klocki", 1.12));
+        this.magazynPrezentow.add(new Prezent("Nerf", 1.67));
+        this.magazynPrezentow.add(new Prezent("Hantle", 3.89));
+    }
 }
